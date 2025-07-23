17112279132a3438efd2681473c387d33ce12dba
KangJS <asdfqaz1996@eco.co.kr>
2025-07-23 17:17:50 +0900

    @mui/icons-material 패키지 추가 및 treeData.json의 한국어 지역화 수정

--- src/TreeView/TreeItem.tsx
+++ src/TreeView/TreeItem.tsx
@@ -1,4 +1,6 @@
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
+import FolderIcon from '@mui/icons-material/Folder';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 
 interface TreeItemData {
   id: string;
@@ -12,8 +14,18 @@ interface TreeItemProps {
 }
 
 export default function TreeItemComponent({ data }: TreeItemProps) {
+  const hasChildren = data.children && data.children.length > 0;
+  
   return (
-    <TreeItem itemId={data.id} label={data.label} disabled={data.disabled}>
+    <TreeItem 
+      itemId={data.id} 
+      label={data.label} 
+      disabled={data.disabled}
+      slots={{
+        collapseIcon: hasChildren ? FolderOpenIcon : undefined,
+        expandIcon: hasChildren ? FolderIcon : undefined,
+      }}
+    >
       {data.children &&
         data.children.map((child) => (
           <TreeItemComponent key={child.id} data={child} />
